737c72ffb098e8f0aff6aceef648c9edd7e7b313
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-05-10 07:44:44 +0600

    show all registered users from database

--- src/pages/Dashboard/UsersPage/UsersPage.js
+++ src/pages/Dashboard/UsersPage/UsersPage.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
 
 const UsersPage = () => {
-
-    return (
-        <div>
-            <h1>users page</h1>
-<h2>h</h2>
-        </div>
-    );
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return (
+    <div>
+      <h2 className="pt-3">All Registered users here</h2>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            
+            <th>#</th>
+            <th>User Name</th>
+            <th>Email Address</th>
+          </tr>
+        </thead>
+        <tbody>
+         {
+             users.map((user,index)=> <tr>
+           
+                <td>{index+1}</td>
+                <td>{user.displayName}</td>
+                <td>{user.email}</td>
+              </tr>)
+         }
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default UsersPage;
